7c264fdb4601290bf49e39b8de04cc2fb1bda75b
Gloriana Fok <gloriana.fok@outlook.com>
2022-08-27 14:48:34 -0400

    push nav menu and routes in App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,25 @@
 import GlobalStyles from "./styles/GlobalStyled";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavMenu from "./NavMenu";
+import Homepage from "../pages/Homepage";
+import MyBookClubs from "../pages/MyBookClubs";
+import BrowseBooks from "../pages/BrowseBooks";
+import Footer from "../components/Footer";
 
 const App = () => {
   return (
     <>
       <GlobalStyles />
       <BrowserRouter>
-        <p>Hello</p>
+        <NavMenu />
+        <Routes>
+          <Route exact path="/" element={<Homepage />}></Route>
+          <Route exact path="/BrowseBooks" element={<BrowseBooks />}></Route>
+          <Route exact path="/MyBookClubs" element={<MyBookClubs />}></Route>
+          {/* <Route exact path="/" element={<UserLogin />}></Route> */}
+          {/* <Route exact path="/" element={<SignUp />}></Route> */}
+        </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   );
---
+++ client/src/components/Footer.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Footer = () => {
+  return <SiteFooter>This is Footer</SiteFooter>;
+};
+
+const SiteFooter = styled.footer`
+  background-color: yellow;
+  border: 2px solid red;
+`;
+
+export default Footer;
