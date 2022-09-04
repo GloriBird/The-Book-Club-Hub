bc0b6cd7ebcf968b3d4ec5a9e0d52d0db7741e35
Gloriana Fok <gloriana.fok@outlook.com>
2022-09-04 16:11:15 -0400

    update request to join

--- server/Routes/currentUser/requestsToJoin.js
+++ server/Routes/currentUser/requestsToJoin.js
@@ -10,48 +10,65 @@ const options = {
 const requestsToJoin = async (req, res) => {
   const client = new MongoClient(MONGO_URI, options);
   await client.connect();
+
   const bookClubData = client.db("Book-Club");
 
   const { username, email, _id, joinedDate, bookClubName, sub } = req.body;
 
-  const getBookClub = await bookClubData.collection("Book-Group").findOne({ bookClubName: bookClubName });
+  const getBookClub = await bookClubData?.collection("Book-Group").findOne({ bookClubName: bookClubName });
+  const profile = await bookClubData?.collection("Users").findOne({ _id: _id });
 
-  const userAlreadyMember = getBookClub?.members.some((match) => _id.includes(match?._id));
-  const userAlreadyPending = getBookClub?.pendingMembers.some((match) => _id.includes(match?._id));
+  const userAlreadyMember = getBookClub?.joinRequestFromUsers.some((match) => _id.includes(match?._id));
+  const userAlreadyPending = getBookClub?.joinRequestFromUsers.some((match) => _id.includes(match?._id));
 
-  const profile = await bookClubData.collection("Users").findOne({ _id: _id });
-  console.log(`profile:`, profile);
+  console.log(`userAlreadyMember:`, userAlreadyMember);
+  console.log(`userAlreadyPending:`, userAlreadyPending);
 
   if (userAlreadyMember === false && userAlreadyPending === false && getBookClub !== null) {
-    const userRequest = await bookClubData.collection("Users").updateOne(
-      { _id: _id },
-      {
-        $push: {
-          bookClubsToJoinPending: {
-            getBookClub,
-          },
-        },
-      }
-    );
+    const requestFromUser = await bookClubData
+      ?.collection("Users")
+      .updateOne({ _id: _id }, { $push: { bookClubsToJoinPending: getBookClub } });
 
-    const bookClubToAccept = await bookClubData.collection("Book-Group").updateOne(
-      { _id: getBookClub._id },
+    const bookClubReceivingRequest = await bookClubData?.collection("Book-Group").updateOne(
+      { _id: getBookClub?._id },
       {
         $push: {
           joinRequestFromUsers: {
-            _id,
-            joinedDate,
             username,
             email,
+            _id,
+            joinedDate,
             sub,
           },
         },
       }
     );
 
-    await bookClubData.collection("Users").updateOne({ _id: _id }, { $inc: { RequestsToJoinCount: 1 } });
+    await bookClubData.collection("Users").updateOne({ _id: _id }, { $inc: { numberOfRequests: 1 } });
 
-    await bookClubData.collection("Book-Group").updateOne({ _id: getBookClub._id }, { $inc: { numberOfRequests: 1 } });
+    await bookClubData
+      .collection("Book-Group")
+      .updateOne({ _id: getBookClub?._id }, { $inc: { numberOfRequestsToJoin: 1 } });
+
+    return (
+      res.status(201).json({
+        status: 201,
+        userRequest: requestFromUser,
+        bookClubRecievedResponse: bookClubReceivingRequest,
+        Message: `Success, request to join is submitted`,
+      }),
+      client.close()
+    );
+  } else if (userAlreadyMember === true || userAlreadyPending === true || getBookClub === null) {
+    return (
+      res.status(409).json({
+        status: 409,
+        userRequest: username,
+        bookClubRecievedResponse: bookClubName,
+        message: `Either this member is already in the bookclub, the book club does not exists, or member is already pending`,
+      }),
+      client.close()
+    );
   }
 };
 
