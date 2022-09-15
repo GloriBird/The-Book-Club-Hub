a99eff32a1d174bb656111a89414c69bb183756c
Gloriana Fok <gloriana.fok@outlook.com>
2022-09-15 19:06:46 -0400

    test

--- client/src/components/BookListInChat.jsx
+++ client/src/components/BookListInChat.jsx
@@ -17,17 +17,17 @@ const BookListInChat = (props) => {
     },
   ];
 
-  const handleRemoveBook = (e) => {
-    // if (isAdded === false) {
-    //   fetch("/remove-books", {
-    //     method: "PATCH",
-    //     headers: { "Content-type": "application/json" },
-    //     body: JSON.stringify({ ...selectedBook, bookClubName: e.target.innerHTML }),
-    //   }).then((response) => {
-    //     return response.json();
-    //   });
-    // }
-  };
+  // const handleRemoveBook = (e) => {
+  //   if (isAdded === false) {
+  //     fetch("/remove-books", {
+  //       method: "PATCH",
+  //       headers: { "Content-type": "application/json" },
+  //       body: JSON.stringify({ ...selectedBook, bookClubName: e.target.innerHTML }),
+  //     }).then((response) => {
+  //       return response.json();
+  //     });
+  //   }
+  // };
 
   return (
     <Wrapper>
